30e5549c45c53e0af163b95a8ad3d699da5f7ec2
damarion989 <d.butlers282@gmail.com>
2026-09-23 17:28:42 -0500

    Implement FileItem class with name and size properties

--- FileItem.java
+++ FileItem.java
@@ -1,1 +1,17 @@
+public class FileItem implements FileSystemItem {
+    private String name;
+    private int sizeInKB;
 
+    public FileItem(String name, int sizeInKB) {
+        this.name = name;
+        this.sizeInKB = sizeInKB;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSizeInKB() {
+        return sizeInKB;
+    }
+}
