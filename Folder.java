87ca9a9af144f4eae2b0fff75c7c2c37930c0efe
damarion989 <d.butlers282@gmail.com>
2026-09-23 17:29:46 -0500

    Implement Folder class for file system structure

--- Folder.java
+++ Folder.java
@@ -1,1 +1,27 @@
+import java.util.ArrayList;
 
+public class Folder implements FileSystemItem {
+    private String name;
+    private ArrayList<FileSystemItem> items;
+
+    public Folder(String name) {
+        this.name = name;
+        items = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSizeInKB() {
+        return 0;
+    }
+
+    public void addItem(FileSystemItem item) {
+        items.add(item);
+    }
+
+    public ArrayList<FileSystemItem> getItems() {
+        return items;
+    }
+}
